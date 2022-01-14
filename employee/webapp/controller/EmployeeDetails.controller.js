96b842d184cd62b2b7ae0a794f5b3b5630fdeb47
lucila isernia <cursosapfiori2021@gmail.com>
2022-01-14 20:27:09 +0000

    deploy

---
+++ employee/webapp/controller/EmployeeDetails.controller.js
@@ -0,0 +1,135 @@
+sap.ui.define([
+    "employee/controller/Base.controller",
+    "employee/model/formatter",
+    "sap/m/MessageBox"
+], function (Base, formatter , MessageBox) {
+
+    function onInit() {
+        this._bus = sap.ui.getCore().getEventBus();
+    };
+
+    function onCreateIncidence() {
+
+        var tableIncidence = this.getView().byId("tableIncidence");
+        var newIncidence = sap.ui.xmlfragment("employee.fragment.NewIncidence", this);
+        var incidenceModel = this.getView().getModel("incidenceModel");
+        var odata = incidenceModel.getData();
+        var index = odata.length;
+        odata.push({ index: index + 1, _ValidateDate: false , EnabledSave:false});
+        incidenceModel.refresh();
+        newIncidence.bindElement("incidenceModel>/" + index);
+        tableIncidence.addContent(newIncidence);
+
+    };
+
+    function onSaveIncidence(oEvent) {
+        var incidence = oEvent.getSource().getParent().getParent();
+        var incidenceRow = incidence.getBindingContext("incidenceModel");
+        this._bus.publish("incidence", "onSaveIncidence", { incidenceRow: incidenceRow.sPath.replace('/', '') });
+    };
+    function onDeleteIncidence(oEvent) {
+
+        var contexjObj = oEvent.getSource().getBindingContext("incidenceModel").getObject();
+
+        MessageBox.confirm(this.getView().getModel("i18n").getResourceBundle().getText("confirmDeleteIncidence"), {
+            onClose: function (oAction) {
+
+                if (oAction === "OK") {
+                    this._bus.publish("incidence", "onDeleteIncidence", {
+                        IncidenceId: contexjObj.IncidenceId,
+                        SapId: contexjObj.SapId,
+                        EmployeeId: contexjObj.EmployeeId
+                    });
+                }
+                
+            }.bind(this)
+        });
+
+    };
+
+    function updateIncidenceCreationDate(oEvent) {
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+
+
+        if (!oEvent.getSource().isValidValue()) {
+            contextObj._ValidateDate = false;
+            contextObj.CreationDateState = "Error";
+
+        } else {
+            contextObj.CreationDateX = true;
+            contextObj._ValidateDate = true;
+            contextObj.CreationDateState = "None";
+
+        };
+
+        if (oEvent.getSource().isValidValue() && contextObj.Reason) {
+            contextObj.EnabledSave = true;
+        } else {
+            contextObj.EnabledSave = false;
+        };
+
+        context.getModel().refresh();
+
+
+
+    };
+
+    function updateIncidenceReason(oEvent) {
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+
+
+        if (oEvent.getSource().getValue()) {
+            contextObj.ReasonX = true;
+            contextObj.ReasonState = "None";
+        } else {
+            contextObj.ReasonState = "Error";
+        };
+
+        if (contextObj._ValidateDate && oEvent.getSource().getValue()) {
+            contextObj.EnabledSave = true;
+        } else {
+            contextObj.EnabledSave = false;
+        };
+
+        context.getModel().refresh();
+    };
+
+    function updateIncidenceType(oEvent) {
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        
+
+        if (contextObj._ValidateDate && contextObj.Reason) {
+            contextObj.EnabledSave = true;
+        } else {
+            contextObj.EnabledSave = false;
+        };
+
+        contextObj.TypeX = true;
+        context.getModel().refresh();
+    };
+
+
+    /*function toOrderDetails(oEvent) {
+        var orderID = oEvent.getSource().getBindingContext("odataNorthwind").getObject().OrderID;
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("RouteOrderDetails", {
+            OrderID : orderID
+        });
+    };*/
+    var EmployeeDetails = Base.extend("employee.controller.EmployeeDetails", {});
+
+    EmployeeDetails.prototype.onInit = onInit;
+    EmployeeDetails.prototype.onCreateIncidence = onCreateIncidence;
+    EmployeeDetails.prototype.onDeleteIncidence = onDeleteIncidence;
+    EmployeeDetails.prototype.Formatter = formatter;
+    EmployeeDetails.prototype.onSaveIncidence = onSaveIncidence;
+    EmployeeDetails.prototype.updateIncidenceCreationDate = updateIncidenceCreationDate;
+    EmployeeDetails.prototype.updateIncidenceReason = updateIncidenceReason;
+    EmployeeDetails.prototype.updateIncidenceType = updateIncidenceType;
+    //EmployeeDetails.prototype.toOrderDetails = toOrderDetails;
+
+    return EmployeeDetails;
+}); 
---
+++ employee/webapp/controller/MasterEmployee.controller.js
@@ -0,0 +1,126 @@
+sap.ui.define([
+    "employee/controller/Base.controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Base, Filter, FilterOperator) {
+        "use strict";
+
+        return Base.extend("employee.controller.MasterEmployee", {
+            onInit: function () {
+               this._bus = sap.ui.getCore().getEventBus();
+
+            },
+
+            onValidate: function () {
+                //obtenemos la referencia al campo de entrada
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                    //inputEmployee.setDescription("OK");
+                    this.byId("labelPaises").setVisible(true);
+                    this.byId("slCountry").setVisible(true);
+
+                } else {
+                    //inputEmployee.setDescription("Not OK");
+                    this.byId("labelPaises").setVisible(false);
+                    this.byId("slCountry").setVisible(false);
+                };
+
+            },
+
+            onFilter:  function(){
+                var oJSON = this.getView().getModel("JsonCountries").getData();
+                var filters = [];
+                        if (oJSON.EmployeeId !== ""){
+                        filters.push(new Filter("EmployeeID" , FilterOperator.EQ, oJSON.EmployeeId))
+                        }     
+                        
+                        if (oJSON.CountryKey !== "" && oJSON.CountryKey !== "undefined" ){
+                            filters.push(new Filter("Country" , FilterOperator.EQ, oJSON.CountryKey))
+                            } 
+
+                           
+                            var oList = this.getView().byId("idEmployeeTable");
+                            var oBinding = oList.getBinding("items");
+                            oBinding.filter(filters);
+                },
+
+                showPostalCode:  function (oEvent){
+
+                    var itemPressed = oEvent.getSource();
+                    var oContext = itemPressed.getBindingContext("JsonEmpleados");
+                    var objectContext = oContext.getObject();
+                 
+                      sap.m.MessageToast.show(objectContext.PostalCode); 
+                 
+                 },
+
+                onClearFilter:  function(){
+                    var oModel = this.getView().getModel("JsonCountries");
+                    oModel.setProperty("/EmployeeId" , "");
+                    oModel.setProperty("/CountryKey" , "");
+                    
+    
+                                
+                    },
+               
+
+
+                    onShowCity: function () {
+                        var oJSONModelConfig = this.getView().getModel("jsonModelConfig");
+                        oJSONModelConfig.setProperty("/visibleCity", true);
+                        oJSONModelConfig.setProperty("/visibleBtnShowCity", false);
+                        oJSONModelConfig.setProperty("/visibleBtnHideCity", true);
+            
+                    },
+            
+                    onHideCity: function (){
+                        var oJSONModelConfig = this.getView().getModel("jsonModelConfig");
+                        oJSONModelConfig.setProperty("/visibleCity", false);
+                        oJSONModelConfig.setProperty("/visibleBtnShowCity", true);
+                        oJSONModelConfig.setProperty("/visibleBtnHideCity", false);
+                    },
+
+                    showOrders: function (oEvent) {
+                         //Get selected Controller
+                            var iconPressed = oEvent.getSource();
+
+                            //Context from the model
+                            var oContext = iconPressed.getBindingContext("odataNorthwind");
+
+                            if (!this._oDialogOrders) {
+                                this._oDialogOrders = sap.ui.xmlfragment("employee.fragment.DialogOrders", this);
+                                this.getView().addDependent(this._oDialogOrders);
+                            };
+
+                            //Dialog binding to the Context to have access to the data of selected item
+                            this._oDialogOrders.bindElement("odataNorthwind>" + oContext.getPath());
+                            this._oDialogOrders.open();
+                    },
+
+                    onCloseOrders:   function () {
+                        this._oDialogOrders.close();
+                    },
+
+                    
+                    showEmployee: function (oEvent) {
+                        var path = oEvent.getSource().getBindingContext("odataNorthwind").getPath();
+                        this._bus.publish("flexible", "showEmployee", path);
+                    },
+
+               /*      toOrderDetails: function(oEvent) {
+                        var orderID = oEvent.getSource().getBindingContext("odataNorthwind").getObject().OrderID;
+                        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                        oRouter.navTo("RouteOrderDetails", {
+                            OrderID : orderID
+                        });
+                    }*/
+
+            
+                });  
+    });
